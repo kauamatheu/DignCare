3533b04c182388327feeb73e113d3bd69a935d1e
kauamatheu <kauamatheu407@gmail.com>
2025-05-18 14:47:39 -0300

    Criação tela depoimentos

---
+++ JS/transicao.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", () => {
+  document.body.classList.add("loaded");
+});
+
+//Quando for utilizar transição cole <script src="/JS/transicao.js" defer></script> no head da página!!!!
