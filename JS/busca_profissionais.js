7d603fc127953c571ae985764093e6dac7297d52
kauamatheu <136603905+kauamatheu@users.noreply.github.com>
2025-05-20 15:26:45 -0300

    Deleção de arquivos javascript inuteis

--- JS/busca_profissionais.js
+++
@@ -1,7 +0,0 @@
-function mudarConteudo() {
-  const valorSelecionado = document.getElementById("seletor").value;
-  const divs = document.querySelectorAll(".conteudo");
-
-  divs.forEach(div => div.classList.remove("ativo"));
-  document.getElementById(valorSelecionado).classList.add("ativo");
-}
