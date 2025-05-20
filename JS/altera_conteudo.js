88b29a8fba8916241d23ade9771099f2d4369213
kauamatheu <136603905+kauamatheu@users.noreply.github.com>
2025-05-20 17:01:12 -0300

    Adicionando comentários nas telas abaixo

--- JS/altera_conteudo.js
+++ JS/altera_conteudo.js
@@ -1,7 +1,12 @@
+//Função para alterar a classe de uma div de acordo com o seletor recebido de um form 
+
 function mudarConteudo() {
+
+  //Variaveis
   const valorSelecionado = document.getElementById("seletor").value;
   const divs = document.querySelectorAll(".conteudo");
 
+  //Remove a classe ativo da div que estava com essa classe e adiciona para a div com o seletor correspondente
   divs.forEach(div => div.classList.remove("ativo"));
   document.getElementById(valorSelecionado).classList.add("ativo");
 }
